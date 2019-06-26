564ede3bf6cc5a83b3159a1126a342cc46ab952c
exsquire <34141973+exsquire@users.noreply.github.com>
2019-06-26 15:17:57 -0700

    Update and rename MaxCores_SingleRun.R to BigBoy.R
    Small Tweaks

--- BigBoy.R
+++ BigBoy.R
@@ -1,25 +1,27 @@
 #Permutation Test - Max Cores, Single Run
-#Rscript portion of Max Core Single Run Design
 library(qtl2)
 
 #Intialize arguments - allow cores to be controlled from SBATCH
-args<-as.integer(commandArgs(TRUE))
+args<-as.integer(unlist(strsplit(commandArgs(TRUE)," ")))
 print(args)
 
-ncores <-args[1]
-print(ncores)
+ncore <- args[1]
+print(ncore)
+
+perms <- args[2]
+print(perms)
 
 #Read in inputs
 apr <- readRDS("./DO2_alleleprob.rds")
-phen <- readRDS("./DO2_permPhenos.rds")
+phen <- readRDS("./samp1000.rds")
 kLOCO <- readRDS("./DO2_kLOCO.rds")
 covar <- readRDS("./addcovar.rds")
 
 #Run the permutation test
-permOut <- scan1perm(apr, phen[,1,drop = F],
+permOut <- scan1perm(apr, phen,
                      kinship = kLOCO,
                      addcovar = covar, 
-                     n_perm = 1000,
+                     n_perm = perms,
                      cores = ncores)
 
 saveRDS(permOut, file = "permOut_SingleRun.rds")
